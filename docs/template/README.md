83759399704354695cba9a82cef0c5258726e8db
peter88213 <petert88213@gmail.com>
2025-12-01 18:22:08 +0100

    Version 5.4.0
    - Using the menu methods provided with novelibre 5.44.
    
    API: 5.44
    Based on novelibre 5.44.3

--- docs/template/README.md
+++ docs/template/README.md
@@ -18,7 +18,7 @@ The [novelibre](https://github.com/peter88213/novelibre/) Python program helps a
 
 ## Requirements
 
-- [novelibre](https://github.com/peter88213/novelibre/) version 5.41+
+- [novelibre](https://github.com/peter88213/novelibre/) version 5.44+
 
 ## Download and install
 
--- src/nv_progress.py
+++ src/nv_progress.py
@@ -15,38 +15,22 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 """
-from pathlib import Path
 import webbrowser
 
 from nvprogress.nvprogress_locale import _
 from nvlib.controller.plugin.plugin_base import PluginBase
 from nvprogress.progress_service import ProgressService
-import tkinter as tk
 
 
 class Plugin(PluginBase):
     """novelibre daily progress log viewer plugin class."""
     VERSION = '@release'
-    API_VERSION = '5.41'
+    API_VERSION = '5.44'
     DESCRIPTION = 'A daily progress log viewer'
     URL = 'https://github.com/peter88213/nv_progress'
     HELP_URL = f'{_("https://peter88213.github.io/nvhelp-en")}/nv_progress'
 
     FEATURE = _('Daily progress log')
-
-    def disable_menu(self):
-        """Disable menu entries when no project is open.
-        
-        Overrides the superclass method.
-        """
-        self._ui.toolsMenu.entryconfig(self.FEATURE, state='disabled')
-
-    def enable_menu(self):
-        """Enable menu entries when a project is open.
-        
-        Overrides the superclass method.
-        """
-        self._ui.toolsMenu.entryconfig(self.FEATURE, state='normal')
 
     def install(self, model, view, controller):
         """Add a submenu to the 'Tools' menu.
@@ -62,35 +46,32 @@ class Plugin(PluginBase):
         self.progressService = ProgressService(model)
         self._icon = self._get_icon('progress.png')
 
+        #--- Configure the main menu.
+
         # Create an entry in the Tools menu.
+        label = self.FEATURE
         self._ui.toolsMenu.add_command(
-            label=self.FEATURE,
+            label=label,
             image=self._icon,
             compound='left',
             command=self.start_viewer,
             state='disabled',
         )
+        self._ui.toolsMenu.disableOnClose.append(label)
 
         # Add an entry to the Help menu.
+        label = _('Progress viewer Online help')
         self._ui.helpMenu.add_command(
-            label=_('Progress viewer Online help'),
+            label=label,
             image=self._icon,
             compound='left',
             command=self.open_help,
         )
 
     def on_close(self):
-        """Close the window.
-        
-        Overrides the superclass method.
-        """
         self.progressService.on_close()
 
     def on_quit(self):
-        """Write back the configuration file.
-        
-        Overrides the superclass method.
-        """
         self.progressService.on_quit()
 
     def open_help(self, event=None):
@@ -99,16 +80,3 @@ class Plugin(PluginBase):
     def start_viewer(self):
         self.progressService.start_viewer(self.FEATURE)
 
-    def _get_icon(self, fileName):
-        # Return the icon for the main view.
-        if self._ctrl.get_preferences().get('large_icons', False):
-            size = 24
-        else:
-            size = 16
-        try:
-            homeDir = str(Path.home()).replace('\\', '/')
-            iconPath = f'{homeDir}/.novx/icons/{size}'
-            icon = tk.PhotoImage(file=f'{iconPath}/{fileName}')
-        except:
-            icon = None
-        return icon
