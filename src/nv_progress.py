2090111b4704f110bc120526456005a2a9ca7fe4
peter88213 <petert88213@gmail.com>
2026-01-12 13:15:59 +0100

    Updated the copyright notice.

--- src/nv_progress.py
+++ src/nv_progress.py
@@ -1,7 +1,7 @@
 """A daily progress log viewer plugin for novelibre.
 
 Requires Python 3.7+
-Copyright (c) 2025 Peter Triesberger
+Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/nv_progress
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 
--- src/nvprogress/progress_view.py
+++ src/nvprogress/progress_view.py
@@ -1,6 +1,6 @@
 """Provide a tkinter widget for progress log display.
 
-Copyright (c) 2025 Peter Triesberger
+Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/nv_progress
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
